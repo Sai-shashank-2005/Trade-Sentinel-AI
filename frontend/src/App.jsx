ccf382d37302f796b11a0ab88971d877cfe7da89
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-02-16 23:55:05 +0530

    Tailwind setup initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <div className="bg-red-500 p-10 rounded-xl shadow-2xl">
+        <h1 className="text-5xl font-bold text-white">
+          Tailwind Working 🚀
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
